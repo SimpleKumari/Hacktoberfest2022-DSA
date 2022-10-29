2c1a362910bddf9b4808f6fb54d349e6d469001b
simple kumari <103820895+SimpleKumari@users.noreply.github.com>
2022-10-29 09:35:19 +0530

    Create queue_concept .py
    queue concept of DSA using list

---
+++ Python/queue_concept .py
@@ -0,0 +1,14 @@
+queue = []
+queue.append('simple')
+queue.append('Himanshu')
+queue.append('Anushka')
+
+print("Queue-1")
+print(queue)
+print("\nElements dequeued from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing elements:")
+print(queue)
